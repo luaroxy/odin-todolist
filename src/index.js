5a3314e1f482375ff4e76bab3ac9bdfcdb9f9855
luaroxy <luannamsiqueira@gmail.com>
2022-07-03 19:41:06 -0600

    Started addTaskForm

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import todoList from './modules/todoPreview.js';
 
 const content = document.getElementById("content");
 
+const addTaskButton = document.getElementById("addTaskButton").addEventListener("click", openAddTaskForm);
+
+function openAddTaskForm() {
+    document.getElementById("addTaskForm").style.display = "block";
+  }
+
 console.log(todoList);
