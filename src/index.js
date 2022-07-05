7f646f37ab7f024cf75e1bf67bd3151109d05f32
luaroxy <luannamsiqueira@gmail.com>
2022-07-05 12:36:39 -0600

    Completed addTaskForm design

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import todoList from './modules/todoPreview.js';
 const content = document.getElementById("content");
 
 const addTaskButton = document.getElementById("addTaskButton").addEventListener("click", openAddTaskForm);
+const closeAddTaskButton = document.getElementById("closeAddTaskButton").addEventListener("click", closeAddTaskForm);
 
 function openAddTaskForm() {
     document.getElementById("addTaskForm").style.display = "block";
-  }
+}
+
+function closeAddTaskForm() {
+  document.getElementById("addTaskForm").style.display = "none";
+}
 
 console.log(todoList);
