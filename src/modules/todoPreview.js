7f0e0c0fea93f7f0ffc979ae186bc7903768ebc3
luaroxy <luannamsiqueira@gmail.com>
2022-07-06 20:35:44 -0600

    Started displayTodoItem logic

--- src/modules/todoPreview.js
+++ src/modules/todoPreview.js
@@ -7,9 +7,24 @@ const addTaskForm = document.getElementById("addTaskForm");
 addTaskForm.addEventListener("submit", function (e) {
 	e.preventDefault();
   todoList.push(createTodoItem());
+  displayTodoItem(todoList);
   addTaskForm.reset();
   closeAddTaskForm();
-  console.log(todoList);
 });
+
+const mainContent = document.getElementById("mainContent");
+
+const projectName = document.createElement('h1');
+projectName.textContent = "Index";
+mainContent.appendChild(projectName);
+
+function displayTodoItem(todoList){
+  todoList.forEach(element => {
+    const todoItem = document.createElement('p');
+    todoItem.textContent = element;
+    mainContent.appendChild(todoItem);
+  });
+}
+
 
 export default todoList;
