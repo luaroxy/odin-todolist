c46709b716ee3657b60ae7d86c3dc43cac49e1bb
luaroxy <luannamsiqueira@gmail.com>
2022-07-15 13:25:11 -0600

    Created todo list usind class and object

---
+++ src/modules/todoList.js
@@ -0,0 +1,23 @@
+export default class TodoList
+{
+    constructor()
+    {
+        this.itemsById = {};
+    }
+    
+    add(newItem)
+    {
+        this.itemsById[newItem.id] = newItem;
+    }
+
+    get items()
+    {
+        return Object.values(this.itemsById);
+    }
+
+
+    removeById(id)
+    {
+        delete this.itemsById[id];
+    }
+}
